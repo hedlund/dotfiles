0f2b3397eefbf7ae568262d3412ed301eef12998
Henrik Hedlund <henrik.hedlund@symfoni.com>
2015-05-01 00:50:45 +0200

    I prefer Total Finder over Xtra Finder after all...

--- cask.sh
+++ cask.sh
@@ -21,7 +21,8 @@ brew tap caskroom/versions
 brew cask install alfred
 brew cask install dropbox
 brew cask install spectacle
-brew cask install xtrafinder
+brew cask install totalfinder
+#brew cask install xtrafinder
 brew cask install iterm2
 brew cask install appzapper
 brew cask install caffeine

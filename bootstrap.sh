00772c524b483cb08819590e676a9ac35e871f9f
Henrik Hedlund <henrik.hedlund@symfoni.com>
2015-05-20 23:20:50 +0200

    Use rcm to bootstrap dotfiles.

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,21 +2,11 @@
 
 cd "$(dirname "${BASH_SOURCE}")";
 
+# Make sure we have the latest goodies
 git pull origin master;
 
-function doIt() {
-	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
-		--exclude "README.md" --exclude "LICENSE-MIT.txt" -avh --no-perms . ~;
-	source ~/.bash_profile;
-}
+# Use rcm to symlink all the dotfiles
+env RCRC=$HOME/.dotfiles/rcrc rcup
 
-if [ "$1" == "--force" -o "$1" == "-f" ]; then
-	doIt;
-else
-	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
-	echo "";
-	if [[ $REPLY =~ ^[Yy]$ ]]; then
-		doIt;
-	fi;
-fi;
-unset doIt;
+# Use mackup to put the Dropboxed config files into place
+#mackup restore

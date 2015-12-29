9aba8624f412d00add1bb4e2141cd8590cfa5cb0
Henrik Hedlund <henrik@hedlund.im>
2015-12-29 17:00:47 +0100

    Make the scripts platform aware.

--- init/personalize.sh
+++ init/personalize.sh
@@ -1,38 +1,25 @@
 #!/usr/bin/env bash
 
-# Install OS X applications for a personal setup.
-# Run this after install-brew.sh and install-cask.sh.
-
-# Ask for the administrator password upfront.
-sudo -v
-
-# Keep-alive: update existing `sudo` time stamp until the script has finished.
-while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
-
 ###############################################################################
-# Install software                                                            #
+# Mac OS X                                                                    #
 ###############################################################################
+if [[ "$(uname -s)" == "Darwin" ]]; then
 
-brew cask install parallels-desktop
-brew cask install intellij-idea-ce
-brew cask install adobe-creative-cloud
-brew cask install steam
+    # Install software
+    brew cask install parallels-desktop
+    brew cask install intellij-idea-ce
+    brew cask install adobe-creative-cloud
+    brew cask install steam
 
-###############################################################################
-# Adobe Lightroom                                                             #
-###############################################################################
+    # Configure Adobe Lightroom
+    # Don't try to import when detecting memory card
+    defaults write com.adobe.Lightroom6 memoryCardDetectionAction -string "ImportBehavior_DoNothing"
+    # Always ask for catalog on startup
+    defaults write com.adobe.Lightroom6 recentLibraryBehavior20 -string "AlwaysPromptForLibrary"
 
-# Don't try to import when detecting memory card
-defaults write com.adobe.Lightroom6 memoryCardDetectionAction -string "ImportBehavior_DoNothing"
-
-# Always ask for catalog on startup
-defaults write com.adobe.Lightroom6 recentLibraryBehavior20 -string "AlwaysPromptForLibrary"
-
-###############################################################################
-# rcup the personal settings                                                  #
-###############################################################################
-
-rcup -t personal
+    # rcup the personal settings
+    rcup -t personal
+fi
 
 ###############################################################################
 # Generate SSH key                                                            #
--- init/workalize.sh
+++ init/workalize.sh
@@ -1,26 +1,17 @@
 #!/usr/bin/env bash
 
-# Install OS X applications for a work setup.
-# Run this after install-brew.sh and install-cask.sh.
-
-# Ask for the administrator password upfront.
-sudo -v
-
-# Keep-alive: update existing `sudo` time stamp until the script has finished.
-while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
-
 ###############################################################################
-# Install software                                                            #
+# Mac OS X                                                                    #
 ###############################################################################
+if [[ "$(uname -s)" == "Darwin" ]]; then
 
-#brew cask install intellij-idea
-brew cask install hipchat
+    # Install software
+    brew cask install hipchat
+    #brew cask install intellij-idea
 
-###############################################################################
-# rcup the work settings                                                      #
-###############################################################################
-
-rcup -t work
+    # rcup the work settings
+    rcup -t work
+fi
 
 ###############################################################################
 # Generate SSH key                                                            #

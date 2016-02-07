eb195350c927af15847b82747aabbf4c1399e1e6
Henrik Hedlund <henrik@hedlund.im>
2016-02-07 13:29:13 +0100

    Making the JS code a bit better structured

---
+++ bin/lib/outdated.js
@@ -0,0 +1,50 @@
+'use strict';
+
+const exec = require('child_process').exec;
+
+function outdatedBrews(callback) {
+    if (callback) {
+        exec('brew outdated', (error, stdout, stderr) => {
+        	const outdated = stdout.toString().split('\n').filter(str => str.trim().length > 0);
+        	callback(outdated);
+        });
+    }
+}
+
+function outdatedCasks(callback) {
+    if (callback) {
+        exec('brew cask list', (error, stdout, stderr) => {
+            let outdated = [];
+            const installed = stdout.toString().trim().split('\n');
+            installed.forEach(app => {
+                exec(`brew cask info ${app}`, (error, stdout, stderr) => {
+                    outdated.push(stdout.toString().includes('Not installed')? app : null);
+                    if (outdated.length === installed.length) {
+                        callback(outdated.filter(Boolean));
+                    }
+                });
+            })
+        });
+    }
+}
+
+function listOutdated(callback, cmd, regex) {
+    if (callback) {
+    	exec(cmd, (error, stdout, stderr) => {
+    		const rows = stdout.toString().split('\n');
+    		const outdated = rows.map(row => {
+    			const match = new RegExp(regex).exec(row);
+    			return match? match[1] : null;
+    		}).filter(Boolean);
+    		callback(outdated);
+    	});
+    }
+}
+
+module.exports = {
+    brew: outdatedBrews,
+    cask: outdatedCasks,
+    sdk: cb => listOutdated(cb, 'source "$HOME/.sdkman/bin/sdkman-init.sh" && sdk outdated', /(\w+)\s\(.*\)/g),
+    npm: cb => listOutdated(cb, 'npm outdated -g --color=false', /^(\w+)\s+\d+\.\d+\.\d+\s+.+\s+.+/g),
+    apm: cb => listOutdated(cb, 'apm outdated --color=false', /── ([\w-]+) .+ -> .+/g)
+};

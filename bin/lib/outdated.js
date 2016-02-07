5f3ba88347d9471ca419f4daf9080128cec14827
Henrik Hedlund <henrik@hedlund.im>
2016-02-07 15:02:22 +0100

    Check the status of the dotfiles themselves.

--- bin/lib/outdated.js
+++ bin/lib/outdated.js
@@ -1,6 +1,39 @@
 'use strict';
 
 const exec = require('child_process').exec;
+const execSync = require('child_process').execSync;
+
+const GitStatus = {
+    UPTODATE: 0,
+    NEEDPULL: 1,
+    NEEDPUSH: 2,
+    DIVERGED: 3
+};
+
+function outdatedDotfiles(callback) {
+    if (callback) {
+        const git = cmd => execSync(`git ${cmd}`, { cwd: __dirname, encoding: 'utf8' });
+
+        git('fetch');
+
+        const local = git('rev-parse @');
+        const remote = git('rev-parse @{u}');
+        const base = git('merge-base @ @{u}');
+
+        if (local === remote) {
+            callback(GitStatus.UPTODATE);
+        }
+        else if (local === base) {
+            callback(GitStatus.NEEDPULL);
+        }
+        else if (remote === base) {
+            callback(GitStatus.NEEDPUSH);
+        }
+        else {
+            callback(GitStatus.DIVERGED);
+        }
+    }
+}
 
 function outdatedBrews(callback) {
     if (callback) {
@@ -42,9 +75,12 @@ function listOutdated(callback, cmd, regex) {
 }
 
 module.exports = {
+    dot: outdatedDotfiles,
     brew: outdatedBrews,
     cask: outdatedCasks,
     sdk: cb => listOutdated(cb, 'source "$HOME/.sdkman/bin/sdkman-init.sh" && sdk outdated', /(\w+)\s\(.*\)/g),
     npm: cb => listOutdated(cb, 'npm outdated -g --color=false', /^(\w+)\s+\d+\.\d+\.\d+\s+.+\s+.+/g),
-    apm: cb => listOutdated(cb, 'apm outdated --color=false', /── ([\w-]+) .+ -> .+/g)
+    apm: cb => listOutdated(cb, 'apm outdated --color=false', /── ([\w-]+) .+ -> .+/g),
+
+    GitStatus: GitStatus
 };

511af498c3c8ec73ecebc362023d47ba3df247d7
Henrik Hedlund <henrik.hedlund@knowit.no>
2017-04-15 22:36:04 +0200

    Fix editorconfig and Code install.

--- init/install-code.sh
+++ init/install-code.sh
@@ -5,6 +5,18 @@ if [ -z "$(which code)" ]; then
     exit 1
 fi
 
+# Install a bunch of useful extensions...
 code --install-extension ms-vscode.cpptools
-code --install-extension dbaeumer.vscode-eslint
 code --install-extension lukehoban.Go
+code --install-extension PeterJausovec.vscode-docker
+code --install-extension felipecaputo.git-project-manager
+code --install-extension dbaeumer.vscode-eslint
+code --install-extension msjsdiag.debugger-for-chrome
+code --install-extension HookyQR.beautify
+code --install-extension donjayamanne.githistory
+code --install-extension humao.rest-client
+code --install-extension EditorConfig.EditorConfig
+code --install-extension alefragnani.Bookmarks
+
+#... and some pretty themes
+code --install-extension file-icons.file-icons

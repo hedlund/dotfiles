337a863f1fe56194369defb8cdfe7b96220987d8
Henrik Hedlund <henrik@hedlund.im>
2017-08-10 20:52:56 +0200

    Tweak the Chocolatey and Visual Studio Code configs

--- init/install-code.sh
+++ init/install-code.sh
@@ -6,17 +6,34 @@ if [ -z "$(which code)" ]; then
 fi
 
 # Install a bunch of useful extensions...
+code --install-extension EditorConfig.EditorConfig
+code --install-extension eamodio.gitlens
+code --install-extension alefragnani.project-manager
+code --install-extension humao.rest-client
+code --install-extension alefragnani.Bookmarks
+code --install-extension formulahendry.auto-close-tag
+code --install-extension formulahendry.auto-rename-tag
+code --install-extension wmaurer.change-case
+code --install-extension christian-kohler.path-intellisense
+code --install-extension cssho.vscode-svgviewer
+code --install-extension bierner.color-info
+code --install-extension mitchdenny.ecdc
+
+# ...a bunch of JavaScript stuff...
+code --install-extension dbaeumer.vscode-eslint
+code --install-extension msjsdiag.debugger-for-chrome
+code --install-extension esbenp.prettier-vscode
+code --install-extension wix.vscode-import-cost
+code --install-extension chrmarti.regex
+code --install-extension christian-kohler.npm-intellisense
+
+# ...some language support...
 code --install-extension ms-vscode.cpptools
 code --install-extension lukehoban.Go
 code --install-extension PeterJausovec.vscode-docker
-code --install-extension felipecaputo.git-project-manager
-code --install-extension dbaeumer.vscode-eslint
-code --install-extension msjsdiag.debugger-for-chrome
-code --install-extension HookyQR.beautify
-code --install-extension donjayamanne.githistory
-code --install-extension humao.rest-client
-code --install-extension EditorConfig.EditorConfig
-code --install-extension alefragnani.Bookmarks
+code --install-extension DotJoshJohnson.xml
+code --install-extension geequlim.godot-tools
 
 #... and some pretty themes
+code --install-extension akamud.vscode-theme-onedark
 code --install-extension file-icons.file-icons

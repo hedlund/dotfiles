6204b1f2dfb270ac5e9165c155904bc0ee72ba16
Henrik Hedlund <henrik@hedlund.im>
2018-12-05 20:11:40 +0100

    Adding bootstrap script for Windows

---
+++ bootstrap.ps1
@@ -0,0 +1,27 @@
+# As we are using mklink (via ln), this script (unfortunately) require elevated privileges
+#Requires -RunAsAdministrator
+
+Function ForceLink([string] $Source, [string] $Target) {
+    if (Test-Path $Target) {
+        Remove-Item -Path $Target
+    }
+    ln -s $Source $Target
+}
+
+# Symlink the few files that makes sense in Windows into place...
+ForceLink -Source "$PSScriptRoot\gitconfig" -Target "$HOME\.gitconfig"
+ForceLink -Source "$PSScriptRoot\hyper.js" -Target "$HOME\.hyper.js"
+ForceLink -Source "$PSScriptRoot\config\vscode-settings.json" -Target "$HOME\AppData\Roaming\Code\User\settings.json"
+
+# Configure Git
+git config --file "$HOME/.gitconfig.local" credential.helper "manager"
+git config --file "$HOME/.gitconfig.local" user.name "Henrik Hedlund"
+
+$confirm = Read-Host -Prompt "Configure Git for personal use? [Y/n]"
+$confirm = $confirm.Trim().ToLower()
+if ($confirm -eq "yes" -or $confirm -eq "y" -or $confirm -eq "") {
+    git config --file "$HOME/.gitconfig.local" user.email "henrik@hedlund.im"
+}
+else {
+    git config --file "$HOME/.gitconfig.local" user.email "henrik.hedlund@remarkable.no"
+}
